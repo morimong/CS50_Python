db8a249c4e41f3172644d073fee1285dbf00e6bb
morimong <a0081353@gmail.com>
2024-04-29 15:00:18 +0900

    submit answer

---
+++ problem-set3/grocery.py
@@ -0,0 +1,20 @@
+import sys
+
+groceries = {}
+
+while True:
+    try:
+        grocery = input("Input: ").upper()
+
+        if grocery in groceries:
+            groceries[grocery] += 1
+        else:
+            groceries[grocery] = 1
+
+        groceries_sorted = sorted(groceries.items(), key=lambda x:x[0])
+        for k, v in groceries_sorted:
+            print(f"{k}: {v}")
+
+    except EOFError:
+        print("\nThank you for coming!")
+        break
